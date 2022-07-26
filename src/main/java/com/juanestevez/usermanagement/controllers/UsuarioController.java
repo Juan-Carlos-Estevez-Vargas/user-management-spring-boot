4fd134f544b93a0caf2fe5ed6d18c21b662ec505
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-07-26 14:58:23 -0500

    odificado de la tabla por usuarios y primer acercamiento al llamado de APIS con AJAX.

--- src/main/java/com/juanestevez/usermanagement/controllers/UsuarioController.java
+++ src/main/java/com/juanestevez/usermanagement/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.juanestevez.usermanagement.controllers;
 
+import com.juanestevez.usermanagement.models.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,8 +10,44 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @GetMapping("/prueba")
-    public List<String> prueba(){
-        return List.of("Manzaba", "kIWI");
+    @GetMapping("/usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Juan");
+        usuario.setApellido("Estevez");
+        usuario.setEmail("juan@example.com");
+        usuario.setTelefono("3213213213");
+        return usuario;
+    }
+
+    @GetMapping("/usuario1")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Juan");
+        usuario.setApellido("Estevez");
+        usuario.setEmail("juan@example.com");
+        usuario.setTelefono("3213213213");
+        return usuario;
+    }
+
+    @GetMapping("/usuario2")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Juan");
+        usuario.setApellido("Estevez");
+        usuario.setEmail("juan@example.com");
+        usuario.setTelefono("3213213213");
+        return usuario;
+    }
+
+    @GetMapping("/usuario3")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Juan");
+        usuario.setApellido("Estevez");
+        usuario.setEmail("juan@example.com");
+        usuario.setTelefono("3213213213");
+        return usuario;
     }
 }
