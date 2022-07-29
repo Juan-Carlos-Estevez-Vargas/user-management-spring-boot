eccb562b4c1dc721d0d1e331cc0375df211a8632
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-07-29 13:40:59 -0500

    Implementación de Argon2 para encriptar las contraseñas.

--- src/main/java/com/juanestevez/usermanagement/controllers/UsuarioController.java
+++ src/main/java/com/juanestevez/usermanagement/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package com.juanestevez.usermanagement.controllers;
 
 import com.juanestevez.usermanagement.dao.IUsuarioDao;
 import com.juanestevez.usermanagement.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,6 +34,9 @@ public class UsuarioController {
 
     @PostMapping("/api/usuarios")
     public void registrarUsuario(@RequestBody Usuario usuario){
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
+        usuario.setPassword(hash);
         usuarioDao.registrar(usuario);
     }
 
